40d5727b065c71bf5c78565fe59e2d8068860dee
EageRAssassin <rt389@cornell.edu>
2017-08-29 22:03:26 -0400

    BuildPost done v 1.0

--- src/cs4321/project1/BuildPostfixExpressionTreeVisitor.java
+++ src/cs4321/project1/BuildPostfixExpressionTreeVisitor.java
@@ -10,43 +10,99 @@ import cs4321.project1.tree.*;
  */
 public class BuildPostfixExpressionTreeVisitor implements TreeVisitor {
 
+	private ListNode node0; // the very first node
+	private ListNode nodex; // the temp node attached after the very first node
+
+	/**
+	 * Method to initialize node0, and nodex
+	 */
 	public BuildPostfixExpressionTreeVisitor() {
-		// TODO fill me in
+		node0 = new NumberListNode(0);
+		nodex = node0;
 	}
-
+	
+	/**
+	 * Method to return the first listNode we generated
+	 * 
+	 * @return the starting node of the whole list, which is the next node of node0
+	 */
 	public ListNode getResult() {
-		// TODO fill me in
-		return null;
+		return node0.getNext();
 	}
-
+	
+	/**
+	 * Method to attach the information of node to nodex in postfix order when visitor is done
+	 * 
+	 * @param the LeafTreeNode to get a postfix pxpression
+	 */
 	@Override
 	public void visit(LeafTreeNode node) {
-		// TODO fill me in
+		nodex.setNext(new NumberListNode(node.getData()));
+		nodex = nodex.getNext();
 	}
-
+	
+	/**
+	 * Method to attach the information of node and its child to nodex in postfix order when visitor is done
+	 * 
+	 * @param the UnaryMinusTreeNode to get a postfix pxpression
+	 */
 	@Override
 	public void visit(UnaryMinusTreeNode node) {
-		// TODO fill me in
+		node.getChild().accept(this);
+		nodex.setNext(new UnaryMinusListNode());
+		nodex = nodex.getNext();
 	}
-
+	
+	/**
+	 * Method to attach the information of node and its child to nodex in postfix order when visitor is done
+	 * 
+	 * @param the AdditionTreeNode to get a postfix pxpression
+	 */
 	@Override
 	public void visit(AdditionTreeNode node) {
-		// TODO fill me in
+		node.getLeftChild().accept(this);
+		node.getRightChild().accept(this);
+		nodex.setNext(new AdditionListNode());
+		nodex = nodex.getNext();
 	}
 
+	/**
+	 * Method to attach the information of node and its child to nodex in postfix order when visitor is done
+	 * 
+	 * @param the MultiplicationTreeNode to get a postfix pxpression
+	 */
 	@Override
 	public void visit(MultiplicationTreeNode node) {
-		// TODO fill me in
+		node.getLeftChild().accept(this);
+		node.getRightChild().accept(this);
+		nodex.setNext(new MultiplicationListNode());
+		nodex = nodex.getNext();
 	}
 
+	/**
+	 * Method to attach the information of node and its child to nodex in postfix order when visitor is done
+	 * 
+	 * @param the SubtractionTreeNode to get a postfix pxpression
+	 */
 	@Override
 	public void visit(SubtractionTreeNode node) {
-		// TODO fill me in
+		node.getLeftChild().accept(this);
+		node.getRightChild().accept(this);
+		nodex.setNext(new MultiplicationListNode());
+		nodex = nodex.getNext();
 	}
 
+	/**
+	 * Method to attach the information of node and its child to nodex when visitor is done
+	 * 
+	 * @param the DivisionTreeNode to get a postfix pxpression
+	 */
 	@Override
 	public void visit(DivisionTreeNode node) {
-		// TODO fill me in
+		node.getLeftChild().accept(this);
+		node.getRightChild().accept(this);
+		nodex.setNext(new DivisionListNode());
+		nodex = nodex.getNext();
 	}
 
 }
