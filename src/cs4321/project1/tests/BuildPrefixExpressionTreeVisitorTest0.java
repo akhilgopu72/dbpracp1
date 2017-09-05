5b0cead3d7ad6878f6ddfc3b7652f958aad98eb1
EageRAssassin <rt389@cornell.edu>
2017-09-04 23:31:04 -0400

    pre ans pour expression test case fixed

--- src/cs4321/project1/tests/BuildPrefixExpressionTreeVisitorTest0.java
+++ src/cs4321/project1/tests/BuildPrefixExpressionTreeVisitorTest0.java
@@ -5,46 +5,38 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import cs4321.project1.BuildPrefixExpressionTreeVisitor;
-import cs4321.project1.list.DivisionListNode;
-import cs4321.project1.list.ListNode;
-import cs4321.project1.list.NumberListNode;
-import cs4321.project1.tree.DivisionTreeNode;
-import cs4321.project1.tree.LeafTreeNode;
-import cs4321.project1.tree.TreeNode;
-
+import cs4321.project1.list.*;
+import cs4321.project1.tree.*;
 public class BuildPrefixExpressionTreeVisitorTest0 {
 
 	private static final double DELTA = 1e-15;
 	
+	//Test Tree: 1-(2*3)
 	@Test
 	public void test() {
+		
 		TreeNode n1 = new LeafTreeNode(1.0);
 		TreeNode n2 = new LeafTreeNode(2.0);
-		TreeNode n3 = new DivisionTreeNode(n1, n2);
-		TreeNode n4 = new DivisionTreeNode(n2, n1);
+		TreeNode n3= new LeafTreeNode(3.0);
+		TreeNode nmuiltiply = new MultiplicationTreeNode(n2, n3);
+		TreeNode nminus = new SubtractionTreeNode(n1, nmuiltiply);
 		
         BuildPrefixExpressionTreeVisitor v1 = new BuildPrefixExpressionTreeVisitor();
-		n3.accept(v1);
+		nminus.accept(v1);
 		ListNode result = v1.getResult();
-		assertTrue(result instanceof DivisionListNode);
+		assertTrue(result instanceof SubtractionListNode);
 		ListNode first = result.getNext();
 		assertTrue(first instanceof NumberListNode);
 		assertEquals(((NumberListNode) first).getData(), 1.0, DELTA);
 		ListNode second = first.getNext();
-		assertTrue(second instanceof NumberListNode);
-		assertEquals(((NumberListNode) second).getData(), 2.0, DELTA);
+		assertTrue(second instanceof MultiplicationListNode);
+		ListNode third = second.getNext();
+		assertTrue(third instanceof NumberListNode);
+		assertEquals(((NumberListNode) third).getData(), 2.0, DELTA);
+		ListNode fourth = third.getNext();
+		assertTrue(fourth instanceof NumberListNode);
+		assertEquals(((NumberListNode) third).getData(), 2.0, DELTA);
 		
-		
-        BuildPrefixExpressionTreeVisitor v2 = new BuildPrefixExpressionTreeVisitor();
-		n4.accept(v2);
-		result = v2.getResult();
-		assertTrue(result instanceof DivisionListNode);
-		first = result.getNext();
-		assertTrue(first instanceof NumberListNode);
-		assertEquals(((NumberListNode) first).getData(), 2.0, DELTA);
-		second = first.getNext();
-		assertTrue(second instanceof NumberListNode);
-		assertEquals(((NumberListNode) second).getData(), 1.0, DELTA);
 	}
 
 }
